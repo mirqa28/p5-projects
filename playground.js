942efea638fe4d044040798ca5cb5339599252ab
Samuel Stanek <samostanek@gmail.com>
2022-07-28 22:33:02 +0200

    A loota stuff
    Co-authored-by: mirqa28 <mirqa28@users.noreply.github.com>
    Co-authored-by: Ivan Kushpel <HollowHero@users.noreply.github.com>

--- playground.js
+++
@@ -1,7 +0,0 @@
-function hw() {
-  console.log("Hello World");
-
-  this.hello = "Hello World";
-}
-
-// object bunch of variables
--- sketch.js
+++ sketch.js
@@ -1,31 +1,41 @@
 class Ball {
-  constructor(x, y, vx, vy, ax, ay) {
-    this.x = x;
-    this.y = y;
-    this.vx = vx;
-    this.vy = vy;
-    this.ax = ax;
-    this.ay = ay;
-  //do not forget to put in all the symbs in bw 1st two brackets
+  constructor(pos, v, a) {
+    this.pos = pos;
+    this.v = v;
+    this.a = a;
+    //do not forget to put in all the symbs in bw 1st two brackets
+    this.material = "steel";
   }
   update() {
     // console.log(this);
-    this.x = this.x + this.vx;
-    this.y = this.y + this.vy;
-    this.vx = this.vx + this.ax;
-    this.vy = this.vy + this.ay; //rychlost poloha = zrychlenie rychlost
+    this.pos.add(this.v);
+    this.v.add(this.a); //rychlost poloha = zrychlenie rychlost
   }
 }
-let ball = new Ball(200, 200, 0, 0, 0, 0); //again.. numbs
+
+const G = 0.01;
+let ball;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
+  ball = new Ball(createVector(200, 200), createVector(0, 0), createVector(0, 0)); //again.. numbs
+  // frameRate(2);
 }
 
 function draw() {
   background('#311465')
-  circle(ball.x, ball.y, 10); //the 10 is a diameter
+  stroke('white');
+  circle(ball.pos.x, ball.pos.y, 10); //the 10 is a diameter
+  const dst = dist(createVector(mouseX, mouseY), ball.pos) ^ 2;
+  const dir = createVector((mouseX - ball.pos.x) / dst, (mouseY - ball.pos.y) / dst);
+  ball.a = createVector(G * dir.x / dst, G * dir.y / dst); //vectors are calculated like this
+  strokeWeight(3);
+  line(ball.pos.x, ball.pos.y, ball.pos.x + ball.v.x * 5, ball.pos.y + ball.v.y * 5);
+  stroke('red');
+  line(ball.pos.x, ball.pos.y, ball.pos.x + ball.a.x * 50, ball.pos.y + ball.a.y * 50);
   ball.update();
-  ball.ax = 10/(mouseX - ball.x);
-  ball.ay = 10/(mouseY - ball.y); //vectors are calculated like this
+}
+
+function dist(a, b) {
+  return sqrt(sq(a.x - b.x) + sq(a.y - b.y));
 }
